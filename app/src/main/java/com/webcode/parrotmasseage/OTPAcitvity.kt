99799792463273770272edf35dfb468a50ceff64
RovayaHaqueShuvo <rovayahaqueshuvo@gmail.com>
2023-10-23 11:01:54 +0600

    Verfication and OTP activity done

---
+++ app/src/main/java/com/webcode/parrotmasseage/OTPAcitvity.kt
@@ -0,0 +1,11 @@
+package com.webcode.parrotmasseage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OTPAcitvity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_otpacitvity)
+    }
+}
